42be5a3d2bc24bc5fb75363a318e26eaf2b7af24
Branden Mosley <branden.mosley1337@gmail.com>
2018-06-19 02:28:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,9 @@ class ApplicationController < Sinatra::Base
   get '/recipes/new' do
     erb :'/recipes/new'
   end
+
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect '/recipes/#{@post.id}'
+  end
 end
